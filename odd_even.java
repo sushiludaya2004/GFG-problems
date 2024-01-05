c07ec75d19a39c19b371c6cc5b0b217a15a4ac50
Sushil <127210875+sushiludaya2004@users.noreply.github.com>
2024-01-05 20:24:12 +0530

    Create odd_even.java

---
+++ odd_even.java
@@ -0,0 +1,8 @@
+//https://www.geeksforgeeks.org/problems/odd-or-even3618/1
+class Solution{
+    static String oddEven(int N){
+        // code here
+        if(N%2 == 0) return "even";
+        else return "odd";
+    }
+}
