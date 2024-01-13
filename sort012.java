004e7c68ef0e8ce6550b86b6153d340bb403179c
Sushil <127210875+sushiludaya2004@users.noreply.github.com>
2024-01-13 17:23:34 +0530

    optimized code

--- sort012.java
+++ sort012.java
@@ -13,16 +13,43 @@ class Solution
         //         }
         //     }
         // }
-        int c0 = 0;
-        int c1 =  0;
-        int c2 = 0;
-        for(int i=0; i<n; i++){
-            if(a[i] == 0) c0++;
-            else if(a[i] == 1) c1++;
-            else c2++;
+        
+        
+        //better solution
+        // int c0 = 0;
+        // int c1 =  0;
+        // int c2 = 0;
+        // for(int i=0; i<n; i++){
+        //     if(a[i] == 0) c0++;
+        //     else if(a[i] == 1) c1++;
+        //     else c2++;
+        // }
+        // for(int i=0; i<c0; i++) a[i] = 0;
+        // for(int i=c0; i<c0+c1; i++) a[i] = 1;
+        // for(int i=c1+c0; i<n; i++) a[i] = 2;
+       
+       
+       //optimized code 
+        int l = 0, m = 0, h = n - 1;
+        while (m <= h) {
+            if (a[m] == 0) {
+                // Swap a[l] and a[m]
+                int temp = a[l];
+                a[l] = a[m];
+                a[m] = temp;
+                l++;
+                m++;
+            } else if (a[m] == 1) {
+                m++;
+            } else {
+                // Swap a[m] and a[h]
+                int temp = a[m];
+                a[m] = a[h];
+                a[h] = temp;
+                h--;
+            }
         }
-        for(int i=0; i<c0; i++) a[i] = 0;
-        for(int i=c0; i<c0+c1; i++) a[i] = 1;
-        for(int i=c1+c0; i<n; i++) a[i] = 2;
+       
+         
     }
 }
