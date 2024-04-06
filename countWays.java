28bf5a0750d6e2adb24afb614f4a09e50f836a33
Sushil <127210875+sushiludaya2004@users.noreply.github.com>
2024-04-06 19:43:35 +0530

    Create countWays.java

---
+++ countWays.java
@@ -0,0 +1,10 @@
+
+//https://www.geeksforgeeks.org/problems/count-ways-to-nth-stairorder-does-not-matter1322/1
+class Solution {
+    // Function to count number of ways to reach the nth stair
+    // when order does not matter.
+    Long countWays(int n) {
+        // your code here
+        return (long)n/2 + 1;
+    }
+}
