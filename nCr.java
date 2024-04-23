fb2b4856c6bcee113fc660b61fc535c5e01e2328
Sushil <127210875+sushiludaya2004@users.noreply.github.com>
2024-04-23 20:29:36 +0530

    Create nCr.java

---
+++ nCr.java
@@ -0,0 +1,25 @@
+
+//https://www.geeksforgeeks.org/problems/ncr1019/1?page=1&difficulty=Medium&status=unsolved&sortBy=submissions
+class Solution{
+    static int mod = (int)1e9 + 7;
+    
+    static int nCr(int n, int r)
+    {
+        // code here
+         if (r > n)
+            return 0;
+        int[] prev = new int[r + 1];
+        int[] curr = new int[r + 1];
+        prev[0] = curr[0] = 1;
+        if (r == n || r == 0) {
+            return 1;
+        }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= r; j++) {
+                curr[j] = (prev[j - 1] + prev[j]) % mod;
+            }
+            prev = curr.clone();
+        }
+        return prev[r];
+    }
+}
