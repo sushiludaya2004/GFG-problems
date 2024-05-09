026faf0aa9ea091d12935eeddeb82030a534b30d
Sushil <127210875+sushiludaya2004@users.noreply.github.com>
2024-05-09 19:11:26 +0530

    Create divisorGame.java

---
+++ divisorGame.java
@@ -0,0 +1,7 @@
+//https://www.geeksforgeeks.org/problems/divisor-game-1664432414/1
+class Solution {
+    public static boolean divisorGame(int n) {
+        // code here
+        return n%2 == 0;
+    }
+}
