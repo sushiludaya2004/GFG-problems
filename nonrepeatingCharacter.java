9ce39ac990b168de5961b1ab33183f06ab2ae154
Sushil <127210875+sushiludaya2004@users.noreply.github.com>
2024-05-16 20:31:14 +0530

    Create nonrepeatingCharacter.java

---
+++ nonrepeatingCharacter.java
@@ -0,0 +1,23 @@
+//https://www.geeksforgeeks.org/problems/non-repeating-character-1587115620/1?page=1&category=Strings&difficulty=Easy&status=unsolved&sortBy=submissions
+class Solution
+{
+    //Function to find the first non-repeating character in a string.
+    static char nonrepeatingCharacter(String S)
+    {
+        //Your code here
+        Map<Character,Integer> m = new HashMap();
+        int n = S.length();
+        for(int i=0;i<n;i++){
+            char ch = S.charAt(i);
+            m.put(ch,m.getOrDefault(ch,0)+1);
+        }
+        for(int i=0;i<n;i++){
+            char ch = S.charAt(i);
+            if(m.get(ch)==1){
+                return ch;
+            }
+        }
+        return '$';
+    }
+}
+
