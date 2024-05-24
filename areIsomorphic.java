02582378998c24072ff4d8d5d76dde82a19ae4c1
Sushil <127210875+sushiludaya2004@users.noreply.github.com>
2024-05-24 13:35:16 +0530

    Create areIsomorphic.java

---
+++ areIsomorphic.java
@@ -0,0 +1,32 @@
+//https://www.geeksforgeeks.org/problems/isomorphic-strings-1587115620/1?page=1&category=Strings&difficulty=Easy&status=unsolved&sortBy=submissions
+class Solution
+{
+    //Function to check if two strings are isomorphic.
+    public static boolean areIsomorphic(String str1,String str2)
+    {
+        // Your code here
+        if(str1.length()!=str2.length()){
+            return false;
+        }
+        // Your code here
+        int s1[]=new int [256];
+        int s2[]=new int [256];
+        Arrays.fill(s1,-1);
+        Arrays.fill(s2,-1);
+        
+        for(int i=0;i<str1.length();i++){
+            char ss=str1.charAt(i);
+            char tt=str2.charAt(i);
+            
+            if(s1[ss]==-1 && s2[tt]==-1){
+                s1[ss]=tt;
+                s2[tt]=ss;
+            }
+            else if(s1[ss]!=tt || s2[tt]!=ss){
+                return false;
+            }
+            
+        }
+        return true;
+    }
+}
